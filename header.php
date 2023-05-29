4d8532831d56bb02440c796997612fa1e448d9df
wei <tk841213@gmail.com>
2023-05-29 16:01:09 +0800

    no message

--- header.php
+++ header.php
@@ -2,7 +2,7 @@
 
 <!-- start -->
 <header class="header">
-    <a href="index.html"class="fa-solid fa-rocket logo" id="logo">
+    <a href="#"class="fa-solid fa-rocket logo" id="logo">
     </a>                
     <a class="fa-solid fa-user-group" id="menu"  ></a>
 </header>
